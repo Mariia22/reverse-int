1bec51c37b33aaaeed6fb87f9fa7c3501359090d
Maria22 <m.ushakova22@gmail.com>
2022-01-04 12:27:07 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result;
+  if (n >= 0) {
+    result =  n.toString().split('').reverse().join('');
+  }
+  else {
+    result = (-1 * n).toString().split('').reverse().join('');
+  }
+
+  return parseInt(result);
 }
